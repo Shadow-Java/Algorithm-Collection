f1b7483ca381a000fecb78e95b58c65935385999
shadow <1723928492@qq.com>
2022-07-14 20:34:40 +0800

    feat(linklist):
    链表相关

---
+++ data-struct-common/src/main/java/Intermittent/lying/continuous/progress/common/datastruct/LinkListNode.java
@@ -0,0 +1,15 @@
+package Intermittent.lying.continuous.progress.common.datastruct;
+
+
+import lombok.Data;
+
+/**
+ *
+ * @author shadow
+ * @Description 单链表节点
+ * @create 2022-07-14 18:39
+ */
+public class LinkListNode {
+    public int value;
+    public LinkListNode next;
+}
--- primary-algorithm/src/main/java/linkedList/LinkedList.java
+++ primary-algorithm/src/main/java/linkedList/LinkedList.java
@@ -1,6 +1,14 @@
 package linkedList;
 
+import Intermittent.lying.continuous.progress.common.datastruct.LinkListNode;
+
 /**
+ *
+ * <ul>
+ *     <li>1.最好不用包装类型，全部用值类型</li>
+ *     <li>2.最好不要用get和set,单值传递</li>
+ *     <li>3.可以设计成一个spring项目,定义一个parent名,每次打包通过这个name</li>
+ * </ul>
  *
  *
  * @author Shadow at 2022/7/11 21:48
@@ -10,15 +18,28 @@ public class LinkedList {
 
 
     /**
-     * 反转链表
-     *
+     * 反转链表,输入头结点，返回头结点
+     * 类型：头结点有值
+     * 比如：5->4->3->2->1
+     * 反转后：1->2->3->4->5
      */
-    public static void reverseList(){
-
+    public static LinkListNode reverseList(LinkListNode head){
+        /**
+         * 代表cur节点的上一个节点
+         */
+        LinkListNode pre = null;
+        /**
+         * 代表cur的下一个节点
+         */
+        LinkListNode next;
+        while(head.next != null){
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
     }
 
-    public static class Node{//单链表
-
-    }
 
 }
