a391304f91f38fbff9f928d2e5a4451e71fcca93
shadow <1723928492@qq.com>
2022-07-26 18:57:59 +0800

    perf(pom):
    uodate plugin

---
+++ algorithm-collection-primary/src/main/java/algorithm/collection/primary/linkedList/Skiplist.java
@@ -0,0 +1,11 @@
+package algorithm.collection.primary.linkedList;
+
+/**
+ * 跳表全称为跳跃列表，它允许快速查询，插入和删除一个有序连续元素的数据链表
+ *
+ * @author shadow
+ * @date 2022/7/26 18:48
+ * @since 1.0
+ */
+public class Skiplist {
+}
