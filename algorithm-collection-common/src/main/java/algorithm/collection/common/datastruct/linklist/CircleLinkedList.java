f18d20e69687f693e76af186854539ca5d8f5863
liyuanbo <2238120323@qq.com>
2023-05-20 11:27:55 +0800

    feat: add todo

---
+++ algorithm-collection-common/src/main/java/algorithm/collection/common/datastruct/linklist/CircleLinkedList.java
@@ -0,0 +1,10 @@
+package algorithm.collection.common.datastruct.linklist;
+
+/**
+ * 环形链表
+ * 环形数组
+ * @author shadow
+ * @create 2023-05-18 01:06
+ **/
+public class CircleLinkedList {
+}
---
+++ algorithm-collection-common/src/main/java/algorithm/collection/common/datastruct/linklist/SkipList.java
@@ -0,0 +1,9 @@
+package algorithm.collection.common.datastruct.linklist;
+
+/**
+ * 跳表
+ * @author shadow
+ * @create 2023-05-18 01:04
+ **/
+public class SkipList {
+}
