a99e87acf90c445d4c08c3ffa79c0f90cefc5845
shadow <1723928492@qq.com>
2023-06-01 11:17:47 +0800

    refactor(pom.xml):

--- algorithm-collection-primary/src/main/java/algorithm/collection/primary/Application.java
+++ algorithm-collection-primary/src/main/java/algorithm/collection/primary/Application.java
@@ -1,11 +1,13 @@
+package algorithm.collection.primary;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
- * TODO
+ * Application
  *
  * @author shadow
  * @date 2023/6/1 10:11
@@ -13,7 +15,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @EnableAsync
 @EnableCaching
-@SpringBootConfiguration
+@SpringBootApplication
 @Slf4j
 public class Application implements CommandLineRunner {
     @Override
