1386a386b60d483a5ddaf18fbc1c36400c0baf6c
shadow <1723928492@qq.com>
2023-06-02 11:42:31 +0800

    feat: 利用jmh进行代码基准测试

--- algorithm-collection-leetcode/src/test/java/algorithm/collection/leetcode/slidingwindow/LongestSubstringTest.java
+++ algorithm-collection-leetcode/src/test/java/algorithm/collection/leetcode/slidingwindow/LongestSubstringTest.java
@@ -4,6 +4,23 @@ package algorithm.collection.leetcode.slidingwindow;
 import algorithm.collection.common.datastruct.tag.DifficultyLevel;
 import algorithm.collection.common.datastruct.tag.QuestionTag;
 import org.junit.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -23,18 +40,28 @@ import org.junit.Test;
         questionTitle = "无重复字符的最长子串",
         relevateClass = LongestSubstring.class
 )
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+@Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@Threads(2)
 class LongestSubstringTest {
 
 
-    @Test
+    @Benchmark
     public void testLengthOfLongestSubstring(){
         String s = "bbbbb";
         System.out.println(lengthOfLongestSubstring(s));
     }
 
-    public static void main(String[] args) {
-        String s = "bbbbb";
-        System.out.println(lengthOfLongestSubstring(s));
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .include(LongestSubstringTest.class.getSimpleName())
+                .resultFormat(ResultFormatType.JSON)
+                .build();
+        new Runner(options).run();
     }
 
     /**
