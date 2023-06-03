44c0ef9df92f50f29acf78c899e30c91b7d12bff
liyuanbo <2238120323@qq.com>
2023-06-03 09:47:43 +0800

    feat: 239.滑动窗口最大值

--- algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/slidingwindow/PermutationString.java
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/slidingwindow/PermutationString.java
@@ -7,20 +7,21 @@ import algorithm.collection.common.datastruct.tag.MethodTag;
 import algorithm.collection.common.datastruct.tag.QuestionTag;
 import algorithm.collection.common.datastruct.tag.SegmentLevel;
 import algorithm.collection.common.datastruct.tag.TimeComplexity;
+
 import java.util.Map;
 
 /**
  * 给你两个字符串s1和s2 ，写一个函数来判断 s2 是否包含 s1的排列。如果是，返回 true ；否则，返回 false 。
- *
+ * <p>
  * 换句话说，s1 的排列之一是 s2 的 子串
- *
+ * <p>
  * 输入：s1 = "ab" s2 = "eidbaooo"
  * 输出：true
  * 解释：s2 包含 s1 的排列之一 ("ba").
- *
+ * <p>
  * 输入：s1= "ab" s2 = "eidboaoo"
  * 输出：false
- *
+ * <p>
  * s1 和 s2 仅包含小写字母
  *
  * @author shadow
@@ -36,15 +37,16 @@ import java.util.Map;
         relevateClass = {MinimumWindowSubstring.class},
         segmentLevel = SegmentLevel.SENSORY_MEMORY,
         timeComplexity = TimeComplexity.O_N,
-        dataStructTypes = {DataStructType.HASH_MAP,DataStructType.ARRAY_LIST}
+        dataStructTypes = {DataStructType.HASH_MAP, DataStructType.ARRAY_LIST}
 )
 public class PermutationString {
 
     /**
      * 使用滑动窗口，窗口的子串就是包含s1的字符，且窗口的长度是固定的
-     *
+     * <p>
      * 1.指针如何移动
      * 2.窗口是如何判断包含s1的子串
+     *
      * @param s1
      * @param s2
      * @return
@@ -76,7 +78,7 @@ public class PermutationString {
         //匹配串需要的字符数 去重的
         int pCount = 0;
         for (int i = 0; i < 26; i++) {
-            if (pFreq[i] > 0){
+            if (pFreq[i] > 0) {
                 pCount++;
             }
         }
@@ -85,25 +87,25 @@ public class PermutationString {
         int right = 0;
         // 当滑动窗口中的字符的种类数与 s1 中对应相等的时候才计数
         int winCount = 0;
-        while (right < tLen){
+        while (right < tLen) {
             //当前的字符中有匹配串
-            if (pFreq[text[right] - 'a'] > 0 ) {
+            if (pFreq[text[right] - 'a'] > 0) {
                 //窗口的字符计数加一
                 winFreq[text[right] - 'a']++;
                 //当滑动窗口中某个字符出现的频率与匹配串字符出现的频率相同则加1
-                if (winFreq[text[right] - 'a'] == pFreq[text[right] - 'a']){
+                if (winFreq[text[right] - 'a'] == pFreq[text[right] - 'a']) {
                     winCount++;
                 }
             }
 
-            while (pCount == winCount){
-                if (right - left +1 == pLen){
+            while (pCount == winCount) {
+                if (right - left + 1 == pLen) {
                     return true;
                 }
                 //当窗口的种类数包含的匹配串的字符种类数，left指针需要向右移动，直到长度一致，这样能保证是连续的子串
-                if (pFreq[text[left] - 'a'] > 0 ) {
+                if (pFreq[text[left] - 'a'] > 0) {
                     winFreq[text[left] - 'a']--;
-                    if (winFreq[text[left] - 'a'] < pFreq[text[left] - 'a']){
+                    if (winFreq[text[left] - 'a'] < pFreq[text[left] - 'a']) {
                         winCount--;
                     }
                 }
@@ -115,41 +117,41 @@ public class PermutationString {
     }
 
     public static void main(String[] args) {
-        String s1 = "aa",s2 = "eidboaoo";
-        boolean res = checkInclusion(s1,s2);
+        String s1 = "aa", s2 = "eidboaoo";
+        boolean res = checkInclusion(s1, s2);
         System.out.println(res);
     }
 
 
     /**
-     *
      * @param s1
      * @param s2
      * @return s1是s2的子串，且长度相等
      */
-    public boolean isSubString(String s1,String s2){
+    public boolean isSubString(String s1, String s2) {
         return true;
     }
 
     /**
      * 窗口s2包含s1的子串：s1的字符种类数等于win的字符种类数，且win的长度等于s1的长度
-     *
+     * <p>
      * ex：s1 = "abc"   win = "abfcd"   统计s2的wincount=s1.length，win的字符个数与s1一致
      * 1.win的字符种类数与匹配串的字符种类数相同(wincount)
      * 2.win的长度和匹配串的长度相同
+     *
      * @param s1
      * @param map
      * @return
      */
-    public static boolean isSubString(String s1,Map<Character,Integer> map){
+    public static boolean isSubString(String s1, Map<Character, Integer> map) {
         int[] book = new int[128];
-        for (char ch: s1.toCharArray()) {
-            book[ch-'a']++;
+        for (char ch : s1.toCharArray()) {
+            book[ch - 'a']++;
         }
-        for (int i=0;i<book.length;i++) {
-            char ch = (char) ('a'+i);
-            if(book[i] >0){
-                if(!map.containsKey(ch) || !map.get(ch).equals(book[i])){
+        for (int i = 0; i < book.length; i++) {
+            char ch = (char) ('a' + i);
+            if (book[i] > 0) {
+                if (!map.containsKey(ch) || !map.get(ch).equals(book[i])) {
                     return false;
                 }
             }
--- algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/slidingwindow/SlidingWindowMaximum.java
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/slidingwindow/SlidingWindowMaximum.java
@@ -3,12 +3,15 @@ package algorithm.collection.leetcode.slidingwindow;
 import algorithm.collection.common.datastruct.tag.AlgorithmCategory;
 import algorithm.collection.common.datastruct.tag.DataStructType;
 import algorithm.collection.common.datastruct.tag.DifficultyLevel;
+import algorithm.collection.common.datastruct.tag.MethodTag;
 import algorithm.collection.common.datastruct.tag.QuestionTag;
 import algorithm.collection.common.datastruct.tag.TimeComplexity;
 
+import java.util.LinkedList;
+
 /**
  * 给你一个整数数组 nums，有一个大小为k的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k个数字。滑动窗口每次只向右移动一位。
- *
+ * <p>
  * 返回 滑动窗口中的最大值 。
  * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
  * 输出：[3,3,5,5,6,7]
@@ -16,12 +19,12 @@ import algorithm.collection.common.datastruct.tag.TimeComplexity;
  * 滑动窗口的位置                最大值
  * ---------------               -----
  * [1  3  -1] -3  5  3  6  7       3
- *  1 [3  -1  -3] 5  3  6  7       3
- *  1  3 [-1  -3  5] 3  6  7       5
- *  1  3  -1 [-3  5  3] 6  7       5
- *  1  3  -1  -3 [5  3  6] 7       6
- *  1  3  -1  -3  5 [3  6  7]      7
- *
+ * 1 [3  -1  -3] 5  3  6  7       3
+ * 1  3 [-1  -3  5] 3  6  7       5
+ * 1  3  -1 [-3  5  3] 6  7       5
+ * 1  3  -1  -3 [5  3  6] 7       6
+ * 1  3  -1  -3  5 [3  6  7]      7
+ * <p>
  * 输入：nums = [1], k = 1
  * 输出：[1]
  *
@@ -36,7 +39,52 @@ import algorithm.collection.common.datastruct.tag.TimeComplexity;
         questionLink = "https://leetcode.cn/problems/sliding-window-maximum/",
         algorithmCategory = AlgorithmCategory.DOUBLE_POINTER,
         timeComplexity = TimeComplexity.O_N,
-        dataStructTypes = {DataStructType.HASH_MAP,DataStructType.ARRAY_LIST}
+        dataStructTypes = {DataStructType.HASH_MAP, DataStructType.ARRAY_LIST}
 )
 public class SlidingWindowMaximum {
+
+    /**
+     * 使用类似单调递减栈结构去推进窗口<br/>
+     * 为什么使用队列：因为需要移除队列头<br/>
+     * 设置双端队列存储最大值（便于O(1)时间获取），队列降序排列，一轮遍历，先把窗口塞满，然后这时就可以返回最大值（队头）了，但返回前要做2件事，首先，要加入数组当前下标到队尾，并保证是单调的，其次，如果队头这是已经到了窗口左边，就移除队头
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    @MethodTag(
+            methodLink = "https://leetcode.cn/problems/sliding-window-maximum/solutions/10025/shuang-xiang-dui-lie-jie-jue-hua-dong-chuang-kou-2/",
+            questionNumber = "239",
+            timeComplexity = TimeComplexity.O_N,
+            dataStructType = DataStructType.DEDED_DOUBLE_QUEUE,
+            algorithmCategory = AlgorithmCategory.DOUBLE_POINTER
+    )
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length < 2) {
+            return nums;
+        }
+        // 双向队列 保存当前窗口最大值的数组位置 保证队列中数组位置的数值按从大到小排序
+        LinkedList<Integer> queue = new LinkedList();
+        // 结果数组
+        int[] result = new int[nums.length - k + 1];
+        // 遍历nums数组
+        for (int i = 0; i < nums.length; i++) {
+            // 保证从大到小 如果前面数小则需要依次弹出，直至满足要求
+            while (!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]) {
+                queue.pollLast();
+            }
+            // 添加当前值对应的数组下标
+            queue.addLast(i);
+            // 判断当前队列中队首的值是否有效,即是否超出窗口
+            if (queue.peek() <= i - k) {
+                queue.poll();
+            }
+            // 当窗口长度为k时 保存当前窗口中最大值
+            if (i + 1 >= k) {
+                result[i + 1 - k] = nums[queue.peek()];
+            }
+        }
+        return result;
+    }
+
 }
