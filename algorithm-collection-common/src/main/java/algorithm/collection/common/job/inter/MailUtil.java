4bc55a8ebdc35dd6952950bde17e1d17245eed74
shadow <1723928492@qq.com>
2023-06-06 15:51:32 +0800

    feat: 新增mail notify

---
+++ algorithm-collection-common/src/main/java/algorithm/collection/common/job/inter/MailUtil.java
@@ -0,0 +1,14 @@
+package algorithm.collection.common.job.inter;
+
+/**
+ * 邮件发送
+ *
+ * @author shadow
+ * @date 2023/6/6 11:08
+ * @since 1.0
+ */
+public interface MailUtil {
+
+    boolean sendMail();
+
+}
