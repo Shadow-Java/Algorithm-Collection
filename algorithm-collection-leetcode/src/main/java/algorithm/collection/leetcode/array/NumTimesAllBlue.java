c7694963698bd07e0b4039a0a4dd8069a0bbd93c
liyuanbo <2238120323@qq.com>
2023-06-14 07:43:04 +0800

    feat: 1375. 二进制字符串前缀一致的次数

---
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/array/NumTimesAllBlue.java
@@ -0,0 +1,91 @@
+package algorithm.collection.leetcode.array;
+
+import algorithm.collection.common.datastruct.tag.AlgorithmCategory;
+import algorithm.collection.common.datastruct.tag.DataStructType;
+import algorithm.collection.common.datastruct.tag.DifficultyLevel;
+import algorithm.collection.common.datastruct.tag.MethodTag;
+import algorithm.collection.common.datastruct.tag.QuestionTag;
+import algorithm.collection.common.datastruct.tag.TimeComplexity;
+import algorithm.collection.primary.tree.DeepWidthSearch;
+
+/**
+ * 给你一个长度为 n 、下标从 1 开始的二进制字符串，所有位最开始都是 0 。我们会按步翻转该二进制字符串的所有位（即，将 0 变为 1）。
+ *
+ * 给你一个下标从 1 开始的整数数组 flips ，其中 flips[i] 表示对应下标 i 的位将会在第 i 步翻转。
+ *
+ * 二进制字符串 前缀一致 需满足：在第 i 步之后，在 闭 区间 [1, i] 内的所有位都是 1 ，而其他位都是 0 。
+ *
+ * 返回二进制字符串在翻转过程中 前缀一致 的次数。
+ *
+ * @author shadow
+ * @create 2023-06-14 07:20
+ **/
+@QuestionTag(
+        difficultyLeve = DifficultyLevel.MEDIUM,
+        questionNumber = "1375",
+        questionTitle = "二进制字符串前缀一致的次数",
+        relevateClass = DeepWidthSearch.class,
+        desc = "给你一个长度为 n 、下标从 1 开始的二进制字符串，所有位最开始都是 0 。我们会按步翻转该二进制字符串的所有位（即，将 0 变为 1）",
+        questionLink = "https://leetcode.cn/problems/number-of-times-binary-string-is-prefix-aligned/",
+        timeComplexity = TimeComplexity.O_N_2,
+        dataStructTypes = {DataStructType.ARRAY_LIST}
+)
+public class NumTimesAllBlue {
+
+    @MethodTag(
+            questionNumber = "1375",
+            methodLink = "https://leetcode.cn/problems/number-of-times-binary-string-is-prefix-aligned/",
+            timeComplexity = TimeComplexity.O_N_2,
+            dataStructType = DataStructType.ARRAY_LIST,
+            algorithmCategory = AlgorithmCategory.VIOLENCE
+    )
+    public int numTimesAllBlue(int[] flips) {
+        int[] arr = new int[flips.length];
+        int count = 0;
+        int max = flips[0];
+        for (int x : flips){
+            arr[x-1] = 1;
+            //最大的下标，就是中间不能为0，比如1101
+            max = Math.max(x,max);
+            if(!exist0(arr,0,max-1)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @return [left,right]是否存在0
+     */
+    private boolean exist0(int[] arr,int left,int right){
+        for (int i=left;i<=right;i++){
+            if(arr[i] == 0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @MethodTag(
+            questionNumber = "1375",
+            methodLink = "https://leetcode.cn/problems/number-of-times-binary-string-is-prefix-aligned/",
+            timeComplexity = TimeComplexity.O_N,
+            dataStructType = DataStructType.ARRAY_LIST,
+            algorithmCategory = AlgorithmCategory.VIOLENCE
+    )
+    public int numTimesAllBlueAnotherWay(int[] flips) {
+        int maxVal=0,n=flips.length,res=0;
+        //当前最大值和下标相等时说明前缀一致
+        for(int i=0;i<n;++i){
+            maxVal=Math.max(maxVal,flips[i]);
+            if(maxVal==i+1)
+                ++res;
+        }
+        return res;
+    }
+
+}
---
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/array/UniqueMorseRepresentations.java
@@ -0,0 +1,79 @@
+package algorithm.collection.leetcode.array;
+
+import algorithm.collection.common.datastruct.tag.AlgorithmCategory;
+import algorithm.collection.common.datastruct.tag.DataStructType;
+import algorithm.collection.common.datastruct.tag.DifficultyLevel;
+import algorithm.collection.common.datastruct.tag.MethodTag;
+import algorithm.collection.common.datastruct.tag.QuestionTag;
+import algorithm.collection.common.datastruct.tag.TimeComplexity;
+import algorithm.collection.primary.tree.DeepWidthSearch;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 国际摩尔斯密码定义一种标准编码方式，将每个字母对应于一个由一系列点和短线组成的字符串， 比如:
+ *
+ * 'a' 对应 ".-" ，
+ * 'b' 对应 "-..." ，
+ * 'c' 对应 "-.-." ，以此类推。
+ * 为了方便，所有 26 个英文字母的摩尔斯密码表如下：
+ *
+ * [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+ * 给你一个字符串数组 words ，每个单词可以写成每个字母对应摩尔斯密码的组合。
+ *
+ * 例如，"cab" 可以写成 "-.-..--..." ，(即 "-.-." + ".-" + "-..." 字符串的结合)。我们将这样一个连接过程称作 单词翻译 。
+ * 对 words 中所有单词进行单词翻译，返回不同 单词翻译 的数量。
+ *
+ * 输入: words = ["gin", "zen", "gig", "msg"]
+ * 输出: 2
+ * 解释:
+ * 各单词翻译如下:
+ * "gin" -> "--...-."
+ * "zen" -> "--...-."
+ * "gig" -> "--...--."
+ * "msg" -> "--...--."
+ *
+ * 共有 2 种不同翻译, "--...-." 和 "--...--.".
+ *
+ * 输入：words = ["a"]
+ * 输出：1
+ *
+ * @author shadow
+ * @create 2023-06-14 06:59
+ **/
+@QuestionTag(
+        difficultyLeve = DifficultyLevel.MEDIUM,
+        questionNumber = "804",
+        questionTitle = "唯一摩尔斯密码词",
+        relevateClass = DeepWidthSearch.class,
+        desc = "国际摩尔斯密码定义一种标准编码方式，将每个字母对应于一个由一系列点和短线组成的字符串",
+        questionLink = "https://leetcode.cn/problems/unique-morse-code-words/",
+        timeComplexity = TimeComplexity.O_N_2,
+        dataStructTypes = {DataStructType.ARRAY_LIST}
+)
+public class UniqueMorseRepresentations {
+
+    @MethodTag(
+            questionNumber = "804",
+            methodLink = "https://leetcode.cn/problems/unique-morse-code-words/",
+            timeComplexity = TimeComplexity.O_N,
+            dataStructType = DataStructType.ARRAY_LIST,
+            algorithmCategory = AlgorithmCategory.VIOLENCE
+    )
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] words_translat = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        //返回不同单词翻译的数量
+        Set<String> hashset = new HashSet<String>();
+        for(int i=0;i<words.length;i++){
+            String word = "";
+            for(int j=0;j<words[i].length();j++){
+                int index = words[i].charAt(j)-97;//如果不知道ASCii值，用words[i].charAt(j) - 'a'
+                word += words_translat[index];
+            }
+            hashset.add(word);
+        }
+        return hashset.size();
+    }
+
+}
