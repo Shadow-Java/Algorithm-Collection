1b67c2a84cee3a711ad72c5932f36c0cda863486
liyuanbo <2238120323@qq.com>
2023-08-22 23:36:06 +0800

    feat: 581. 最短无序连续子数组

--- algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/doublepointer/ShortestUnsortedContinuousSubarray.java
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/doublepointer/ShortestUnsortedContinuousSubarray.java
@@ -66,4 +66,39 @@ public class ShortestUnsortedContinuousSubarray {
         return right - left > 0 ? right-left+1 : 0;
     }
 
+    /**
+     * <pre>
+     *    这种区间将数组分为左段、中段和右段，左段和右段都是升序排列，中段是无序<br/>
+     *    如果进入了右段，就没有比最大值小的数，所以最后一个比最大值小的数就是中段的右边界;
+     *    同理，如果进入左段，就不会出现比最小值更大的情况，所以最后一个出现就视为中段左边界.
+     * </pre>
+     * @param nums
+     * @return
+     */
+    public int anotherWay(int[] nums) {
+        //初始化
+        int len = nums.length;
+        int min = nums[len-1];
+        int max = nums[0];
+        int begin = 0;
+        int end = -1;
+        //遍历
+        for(int i = 0; i < len; i++){
+            if(nums[i] < max){      //从左到右维持最大值，寻找右边界end
+                end = i;
+            }else{
+                max = nums[i];
+            }
+
+            if(nums[len-i-1] > min){    //从右到左维持最小值，寻找左边界begin
+                begin = len-i-1;
+            }else{
+                min = nums[len-i-1];
+            }
+        }
+        return end-begin+1;
+    }
+
+
+
 }
