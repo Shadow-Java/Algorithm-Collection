8b3a51aab627731b7cd0703dd5168232b8a056a2
liyuanbo <2238120323@qq.com>
2024-03-17 20:07:54 +0800

    feat: 124.树的最小高度

--- algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/tree/dfs/MinHeightTree.java
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/tree/dfs/MinHeightTree.java
@@ -23,11 +23,13 @@ public class MinHeightTree {
     private static List<Integer> visited = new ArrayList<>();
 
     public static void main(String[] args) {
-        int[][] edges = new int[3][2];
-        edges[0] = new int[]{1,0};
-        edges[1] = new int[]{1,2};
-        edges[2] = new int[]{1,3};
-        findMinHeightTrees(4,edges);
+        int[][] edges = new int[5][2];
+        edges[0] = new int[]{3,0};
+        edges[1] = new int[]{3,1};
+        edges[2] = new int[]{3,2};
+        edges[3] = new int[]{3,4};
+        edges[4] = new int[]{5,4};
+        findMinHeightTrees(6,edges);
     }
 
     public static List<Integer> findMinHeightTrees(int n, int[][] edges) {
@@ -59,9 +61,11 @@ public class MinHeightTree {
     public static void dfs(int[][] edges,Integer i,Map<Integer,List<Integer>> listMap,int curNode) {
         visited.add(i);
         curBranchHeight++;
+        minHeight = Math.max(minHeight,curBranchHeight);
         System.out.println("访问节点值为："+i);
         if(visited.size() == num) {
-            Optional<Integer> optionalInteger = listMap.keySet().stream().filter(val->val <= minHeight).findFirst();
+            System.out.println("访问完成后，当前分支高度"+curBranchHeight+"当前树最大高度："+minHeight);
+            Optional<Integer> optionalInteger = listMap.keySet().stream().filter(val->val < minHeight).findFirst();
             if(optionalInteger.isEmpty()) {
                 List<Integer> list = listMap.getOrDefault(minHeight,new ArrayList<>());
                 list.add(curNode);
@@ -76,18 +80,17 @@ public class MinHeightTree {
                 dfs(edges,edges[row][1],listMap,curNode);
                 //这里minHeight表示的是当前深度分支的高度，而不是以curNode为顶点的最小高度，可能存在当前分支高度为3，另一个分支高度为4
                 //这里回退的高度为当前分支的高度，退一个节点回退一个高度
+                System.out.println("回退到节点值为："+edges[row][1]+",树高度为："+minHeight+"，当前分支高度为："+curBranchHeight);
                 curBranchHeight--;
-                System.out.println("回退到节点值为："+edges[row][1]+",当前分支高度为："+curBranchHeight);
                 //这里会回溯找其他节点
             } else if(edges[row][1] == i && !visited.contains(edges[row][0])) {
                 dfs(edges,edges[row][0],listMap,curNode);
                 //这里会回溯找其他节点，即回溯row点，继续遍历row+1点
+                System.out.println("回退到节点值为："+edges[row][0]+",树高度为："+minHeight+"，当前分支高度为："+curBranchHeight);
                 curBranchHeight--;
-                System.out.println("回退到节点值为："+edges[row][0]+",当前分支高度为："+curBranchHeight);
             }
         }
         //走到这个区域意味着一个深度分支遍历完毕
-        minHeight = Math.max(minHeight,curBranchHeight);
         System.out.println("以"+curNode+"为顶点"+"，当前节点值为"+i+"的节点分支深度遍历完毕，树深度为"+curBranchHeight+"，当前分支高度为"+curBranchHeight);
     }
 
