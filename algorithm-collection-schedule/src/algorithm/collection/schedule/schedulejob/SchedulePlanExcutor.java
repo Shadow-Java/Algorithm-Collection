9d917b1f9f382588b79b0635cd902a7b7df569c1
liyb <1723928492@qq.com>
2024-10-25 17:08:23 +0800

    feat: add learning plan

---
+++ algorithm-collection-schedule/src/algorithm/collection/schedule/schedulejob/SchedulePlanExcutor.java
@@ -0,0 +1,22 @@
+package algorithm.collection.schedule.schedulejob;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SchedulePlanExcutor {
+
+
+    public static void main(String[] args) {
+        List<String> items = new ArrayList<>();
+    }
+
+
+    public void registerItem_10_25(String item) {
+        List<String> items = List.of("");
+    }
+
+    public void registerItem_10_26(String item) {
+        List<String> items = List.of("");
+    }
+
+}
