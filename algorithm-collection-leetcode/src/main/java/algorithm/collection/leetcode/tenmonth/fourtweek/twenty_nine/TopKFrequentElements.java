05ef59e21f49ed0b695ce865e751b5bc4a53a014
liyb <1723928492@qq.com>
2024-10-29 19:26:39 +0800

    feat: 10.29 第K大的字符串

---
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/tenmonth/fourtweek/twenty_nine/TopKFrequentElements.java
@@ -0,0 +1,17 @@
+package algorithm.collection.leetcode.tenmonth.fourtweek.twenty_nine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TopKFrequentElements {
+
+    /**
+     * 第K大的字符串，即出现次数最多的前K个字符串
+     * @param strings
+     * @param k
+     * @return
+     */
+    public List<String> topK(List<String> strings,int k) {
+        return new ArrayList<>();
+    }
+}
