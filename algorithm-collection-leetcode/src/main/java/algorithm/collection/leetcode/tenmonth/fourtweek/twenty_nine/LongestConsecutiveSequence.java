bbe37b0cce8670a4f92cad9521cb03f55ec46d5b
liyb <1723928492@qq.com>
2024-10-31 17:09:00 +0800

    refactor: 滑动窗口

--- algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/tenmonth/fourtweek/twenty_nine/LongestConsecutiveSequence.java
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/tenmonth/fourtweek/twenty_nine/LongestConsecutiveSequence.java
@@ -24,6 +24,7 @@ public class LongestConsecutiveSequence {
         int max = 1;
         Set<Integer> windowSet = new HashSet<>();
         windowSet.add(nums[0]);
+        //不定长窗口，只有窗口没有left，即只需要更新窗口即可
         for (int right =1;right< nums.length;right++) {
             windowSet.add(nums[right]);
             if(nums[right-1]+1 != nums[right] && nums[right-1] != nums[right]) {
@@ -38,6 +39,11 @@ public class LongestConsecutiveSequence {
 
     public static void main(String[] args) {
         int[] nums = {100, 4, 200, 1, 3, 2};
+        /**
+         * 两种思路：
+         *    1.滑动窗口或者双指针
+         *    2.使用hashMap：判断是否是开头值，不是则跳过，是的话则轮询查下一个值是否存在
+         */
         System.out.println(longestConsecutive(nums));
     }
 
