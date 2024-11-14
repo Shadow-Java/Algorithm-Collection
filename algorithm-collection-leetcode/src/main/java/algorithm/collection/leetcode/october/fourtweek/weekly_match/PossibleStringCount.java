70b3179df312144e9dea9db4dbbf519ff3ecbc7b
liyb <1723928492@qq.com>
2024-11-14 18:32:42 +0800

    refactor: 3330. 找到初始输入字符串 I

--- algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/october/fourtweek/weekly_match/PossibleStringCount.java
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/october/fourtweek/weekly_match/PossibleStringCount.java
@@ -1,5 +1,10 @@
 package algorithm.collection.leetcode.october.fourtweek.weekly_match;
 
+import algorithm.collection.common.datastruct.tag.*;
+import algorithm.collection.leetcode.slidingwindow.PermutationString;
+import algorithm.collection.leetcode.tree.dfs.MaxDepth;
+import algorithm.collection.primary.tree.DeepWidthSearch;
+
 /**
  * 3330. 找到初始输入字符串 I
  * Alice 正在她的电脑上输入一个字符串。但是她打字技术比较笨拙，她 可能 在一个按键上按太久，导致一个字符被输入 多次 。
@@ -25,11 +30,28 @@ package algorithm.collection.leetcode.october.fourtweek.weekly_match;
  * @author shadow
  * @create 2024-10-30 00:29
  **/
+@QuestionTag(
+        questionNumber = "3330. 找到初始输入字符串 I",
+        questionTitle = "找到初始输入字符串 I",
+        relevateClass = {PermutationString.class, MaxDepth.class, DeepWidthSearch.class},
+        difficultyLeve = DifficultyLevel.MEDIUM,
+        desc = "Alice 正在她的电脑上输入一个字符串。但是她打字技术比较笨拙，她 可能 在一个按键上按太久，导致一个字符被输入 多次",
+        algorithmCategory = AlgorithmCategory.BACK_TRACKING,
+        timeComplexity = TimeComplexity.O_N,
+        dataStructTypes = {DataStructType.ARRAY_LIST}
+)
 public class PossibleStringCount {
 
 
     /**
      * 她可能在一个按键上按太久，导致一个字符被输入多次；表示一个字母可能被输入了多次，而不只是一次；比如a -> aaaa(多输入了三次)
+     *
+     * dp为了处理边界情况，通常比平时多一个空间；for的遍历是从1到n，是对dp的，但相对于word是dp[i]对于word.charAt(i - 1);
+     * 所以i表示dp的前i个字符的总方案数，第i个字符为word.charAt(i - 1)；
+     *
+     * 对于当前i：
+     *        1.不选第i个字符，则dp[i] = dp[i - 1]
+     *        2.选择第i个字符，则比较当前字符是否与上一个字符相同，相同则
      * @param word
      * @return
      */
