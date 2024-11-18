a43eaaf59ea22c8eaa298f4b5c7e633a30c03790
liyb <1723928492@qq.com>
2024-11-18 10:06:46 +0800

    refactor: todo list

--- algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/november/secondweek/daily/Demo.java
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/november/secondweek/daily/Demo.java
@@ -1,4 +1,5 @@
 package algorithm.collection.leetcode.november.secondweek.daily;
 
 public class Demo {
+    //TODO 补充七天的打卡记录
 }
---
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/november/secondweek/november_17st/Demo2.java
@@ -0,0 +1,5 @@
+package algorithm.collection.leetcode.november.secondweek.november_17st;
+
+public class Demo2 {
+    //TODO 17号的题解
+}
