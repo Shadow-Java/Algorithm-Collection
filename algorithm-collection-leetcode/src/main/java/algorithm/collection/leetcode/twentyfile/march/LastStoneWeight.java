28461e880e69cabc316c0fbd521ffb7150f61608
liyb <1723928492@qq.com>
2025-03-19 18:31:39 +0800

    feat: LeetCode 1046. Last Stone Weight

--- algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/twentyfile/march/LastStoneWeight.java
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/twentyfile/march/LastStoneWeight.java
@@ -1,6 +1,5 @@
 package algorithm.collection.leetcode.twentyfile.march;
 
-import java.util.Collections;
 import java.util.PriorityQueue;
 
 /**
@@ -10,22 +9,18 @@ import java.util.PriorityQueue;
 public class LastStoneWeight {
 
     public int lastStoneWeight(int[] stones) {
-        // 创建最大堆
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for (int stone : stones) {
-            maxHeap.offer(stone);
+        //创建最大堆
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(((o1, o2) -> o2-o1));
+        for (int item : stones) {
+            maxHeap.offer(item);
         }
-
-        // 模拟操作
         while (maxHeap.size() > 1) {
-            int stone1 = maxHeap.poll(); // 取出最大的石头
-            int stone2 = maxHeap.poll(); // 取出第二大的石头
-            if (stone1 != stone2) {
-                maxHeap.offer(stone1 - stone2); // 将差值加入堆
+            int max = maxHeap.poll();
+            int min = maxHeap.poll();
+            if(max > min) {//判断是否相等
+                maxHeap.offer(max-min);
             }
         }
-
-        // 返回结果
         return maxHeap.isEmpty() ? 0 : maxHeap.poll();
     }
 
