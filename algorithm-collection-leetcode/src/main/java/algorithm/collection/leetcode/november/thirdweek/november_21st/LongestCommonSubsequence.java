cc645271603bf284739f3bc321bc4a48afbd8e6c
liyuanbo <2238120323@qq.com>
2025-03-24 21:11:09 +0800

    1143. 最长公共子序列

--- algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/november/thirdweek/november_21st/LongestCommonSubsequence.java
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/november/thirdweek/november_21st/LongestCommonSubsequence.java
@@ -1,5 +1,7 @@
 package algorithm.collection.leetcode.november.thirdweek.november_21st;
 
+import java.util.Arrays;
+
 /**
  * 1143. 最长公共子序列
  * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
@@ -33,7 +35,37 @@ public class LongestCommonSubsequence {
      * @return
      */
     public int longestCommonSubsequence(String text1, String text2) {
-        return 0;
+        int n = text1.length();
+        int m = text2.length();
+        memo = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(memo[i],Integer.MIN_VALUE);
+        }
+        return dfs(n - 1, m - 1, text1, text2);
+    }
+
+    public int[][] memo;
+
+    /**
+     * 求的是i长度的字符串和j长度的字符串的公共子序列的长度
+     * @param i       当临界值到达0 的时候，此时公共子序列的长度为0
+     * @param j       当临界值到达0 的时候，此时公共子序列的长度为0
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public int dfs(int i, int j, String text1, String text2) {
+        if (i < 0 || j < 0) {
+            //代表两个长度为0的字符串的最长公共子序列的长度为0
+            return 0;
+        }
+        if(memo[i][j] != Integer.MIN_VALUE) {
+            return memo[i][j];
+        }
+        if (text1.charAt(i) == text2.charAt(j)) {//求的是两个字符的公共子序列，即只有相同时才
+            return memo[i][j] = dfs(i - 1, j - 1, text1, text2) + 1;
+        }
+        return memo[i][j] = Math.max(dfs(i - 1, j, text1, text2), dfs(i, j - 1, text1, text2));
     }
 
 }
