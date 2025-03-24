e678e258d3f461851f6bb82bb3fcf651a9446c17
liyb <1723928492@qq.com>
2025-03-24 18:36:13 +0800

    feat: 给一个字符串，使其重新排列两个字符间的字符不重复，能排列输出yes ，并给出排列字符。不能排列请输出no

---
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/november/thirdweek/november_21st/LongestCommonSubstring.java
@@ -0,0 +1,9 @@
+package algorithm.collection.leetcode.november.thirdweek.november_21st;
+
+/**
+ * 最长公共子串
+ * 子串和子数组是连续的
+ * 子序列是可以不连续的
+ */
+public class LongestCommonSubstring {
+}
---
+++ algorithm-collection-leetcode/src/main/java/algorithm/collection/leetcode/november/thirdweek/november_21st/RearrangeString.java
@@ -0,0 +1,7 @@
+package algorithm.collection.leetcode.november.thirdweek.november_21st;
+
+/**
+ * 给一个字符串，使其重新排列两个字符间的字符不重复，能排列输出yes ，并给出排列字符。不能排列请输出no
+ */
+public class RearrangeString {
+}
